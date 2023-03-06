bdd0daea36ee4a7423136778550666a023da15a6
Anuka <arrinsamurai7@gmail.com>
2023-03-06 22:52:31 +0530

    fist commit

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  PATH_OF_API = 'http://localhost:8080/api/auth';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(
+    private httpclient: HttpClient,
+  ) {}
+
+
+  login(loginData): Observable<any> {
+    return this.httpclient.post(
+      this.PATH_OF_API + '/signin',
+      loginData,
+      this.httpOptions
+    );
+  }
+
+  public forUser() {
+    return this.httpclient.get(this.PATH_OF_API + '/forUser', {
+      responseType: 'text',
+    });
+  }
+
+  public forAdmin() {
+    return this.httpclient.get(this.PATH_OF_API + '/forAdmin', {
+      responseType: 'text',
+    });
+  }
+
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserAuthService } from '../_services/user-auth.service';
+import { UserService } from '../_services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  constructor(
+    private userService: UserService,
+    private userAuthService: UserAuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  login(loginForm: NgForm) {
+
+    this.userService.login(loginForm.value).subscribe(
+      (response: any) => {
+        this.userAuthService.setRoles(response.roles);
+        this.userAuthService.setToken(response.token);
+        this.userAuthService.setId(response.id);
+        
+        const role = response.roles[0];
+        if (role === 'ROLE_ADMIN') {
+          this.router.navigate(['/admin']);
+        } else if (role === 'ROLE_PROVIDER') {
+          this.router.navigate(['/user']);
+        } else {
+          this.router.navigate(['/user']);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
