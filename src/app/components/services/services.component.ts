aaefccdbc285962ea1ee6099db62a00c8e1ee589
Anuka <arrinsamurai7@gmail.com>
2023-03-14 10:51:41 +0530

    service cards

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Service } from 'src/app/model/Service';
+import { ServiceService } from 'src/app/services/service.service';
 
 @Component({
   selector: 'app-services',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
+  ServiceList!: Service[];
+  deletingExerciseId: string;
+
+  constructor(
+   private serviceService : ServiceService
+  ) { }
 
   ngOnInit(): void {
+    this.getAllServices();
   }
 
+  getAllServices() {
+    this.serviceService.getAllServices().subscribe((response: any) => {
+      this.ServiceList = response;
+    });
+  }
+
+  getRandomColor() {
+    const colors = ['#FF5733', '#C70039', '#900C3F', '#581845', '#FFC300', '#DAF7A6'];
+    const randomColor1 = colors[Math.floor(Math.random() * colors.length)];
+    const randomColor2 = colors[Math.floor(Math.random() * colors.length)];
+    return `${randomColor1}, ${randomColor2}`;
+  }
 }
---
+++ src/app/services/service.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Service } from '../model/Service';
+import { catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+providedIn: 'root'
+})
+
+export class ServiceService {
+
+private PATH_OF_SERVICE = 'http://localhost:8080/home/services';
+
+constructor(private httpClient: HttpClient) { }
+
+// getServiceList(): Observable<Service[]> {
+//   return this.httpClient.get<Service[]>(`${this.PATH_OF_SERVICE}/all`);
+// }
+
+getAllServices() {
+  return this.httpClient.get(`${this.PATH_OF_SERVICE}/all`);
+}
+
+getServiceById(id: String) {
+  return this.httpClient.get(`${this.PATH_OF_SERVICE}/id/${id}`);
+}
+
+createService(service: Service) {
+  return this.httpClient.post<Response>(this.PATH_OF_SERVICE, service).pipe(
+    tap((response: Response) => {
+      console.log(response);
+    }),
+    catchError((error) => {
+      console.log(error);
+      return of(null);
+    })
+  );
+}
+
+updateService(id: string, service: Service) {
+  return this.httpClient.put(`${this.PATH_OF_SERVICE}/${id}`, service);
+}
+
+deleteService(id: string) {
+  return this.httpClient.delete(`${this.PATH_OF_SERVICE}/${id}`);
+}
+
+// getServiceById(id: string): Observable<Service> {
+// return this.http.get<Service>(${this.baseUrl}/id/${id});
+// }
+
+// createService(service: Service): Observable<Service> {
+// return this.http.post<Service>(${this.baseUrl}, service);
+// }
+
+// updateService(id: string, service: Service): Observable<Service> {
+// return this.http.put<Service>(${this.baseUrl}/${id}, service);
+// }
+
+// deleteService(id: string): Observable<any> {
+// return this.http.delete(${this.baseUrl}/${id}, { responseType: 'text' });
+// }
+}
